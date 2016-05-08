27e7c667d5a161b99ecd072b698891ed739b27ee
TechBytes with Rohit <rohit1290@users.noreply.github.com>
2016-05-08 22:02:35 +0530

    Update create.php

--- views/default/river/object/thewire/create.php
+++ views/default/river/object/thewire/create.php
@@ -1,39 +1,68 @@
+
+text/x-generic create.php ( PHP script text )
+
+<?php
+/**
+ * File river view.
+ */
+
 $object = $vars["item"]->getObjectEntity();
 $excerpt = strip_tags($object->description);
 
 $attachments = "";
-$reshare = $object->getEntitiesFromRelationship(array("relationship" => "reshare", "limit" => 1));
-if (!empty($reshare)) {
-    $attachments = elgg_view("thewire_tools/reshare_source", array("entity" => $reshare[0]));
+
+if(elgg_is_active_plugin("thewire_tools") && elgg_is_active_plugin("elgg-thewire-links-preview")){
+	$reshare = $object->getEntitiesFromRelationship(array("relationship" => "reshare", "limit" => 1));
+	if (!empty($reshare)) {
+		$attachments = elgg_view("thewire_tools/reshare_source", array("entity" => $reshare[0]));
+	}
+
+	if($attachments==""){
+	$excerpt = elgg_preview($excerpt);
+	$excerpt = thewire_filter($excerpt);
+	}else{
+	$excerpt = thewire_tools_filter($excerpt);
+	}
 }
 
-if($attachments==""){
-$excerpt = elgg_preview($excerpt);
-$excerpt = thewire_filter($excerpt);
-}else{
-$excerpt = thewire_tools_filter($excerpt);
+if(elgg_is_active_plugin("thewire_tools") && !elgg_is_active_plugin("elgg-thewire-links-preview")){
+	$attachments = "";
+	$reshare = $object->getEntitiesFromRelationship(array("relationship" => "reshare", "limit" => 1));
+	if (!empty($reshare)) {
+		$attachments = elgg_view("thewire_tools/reshare_source", array("entity" => $reshare[0]));
+	}
+	$excerpt = thewire_tools_filter($excerpt);
+}
+
+if(elgg_is_active_plugin("elgg-thewire-links-preview") && !elgg_is_active_plugin("thewire_tools")){
+	$excerpt = elgg_preview($excerpt);
+	$excerpt = thewire_filter($excerpt);	
+}
+
+if(!elgg_is_active_plugin("elgg-thewire-links-preview") && !elgg_is_active_plugin("thewire_tools")){
+	$excerpt = thewire_filter($excerpt);	
 }
 
 $subject = $vars["item"]->getSubjectEntity();
 $subject_link = elgg_view("output/url", array(
-    "href" => $subject->getURL(),
-    "text" => $subject->name,
-    "class" => "elgg-river-subject",
-    "is_trusted" => true,
+	"href" => $subject->getURL(),
+	"text" => $subject->name,
+	"class" => "elgg-river-subject",
+	"is_trusted" => true,
 ));
 
 $object_link = elgg_view("output/url", array(
-    "href" => "thewire/owner/$subject->username",
-    "text" => elgg_echo("thewire:wire"),
-    "class" => "elgg-river-object",
-    "is_trusted" => true,
+	"href" => "thewire/owner/$subject->username",
+	"text" => elgg_echo("thewire:wire"),
+	"class" => "elgg-river-object",
+	"is_trusted" => true,
 ));
 
 $summary = elgg_echo("river:create:object:thewire", array($subject_link, $object_link));
 
 echo elgg_view("river/elements/layout", array(
-    "item" => $vars["item"],
-    "message" => $excerpt,
-    "summary" => $summary,
-    "attachments" => $attachments
+	"item" => $vars["item"],
+	"message" => $excerpt,
+	"summary" => $summary,
+	"attachments" => $attachments
 ));
