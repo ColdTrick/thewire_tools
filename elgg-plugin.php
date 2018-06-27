8064bfd96a81cab6dd61436d9256ff77b9d2fa75
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-06-27 10:34:28 +0200

    changed: updated settings form

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -4,6 +4,13 @@ require_once(dirname(__FILE__) . '/lib/functions.php');
 
 return [
 	'bootstrap' => '\ColdTrick\TheWireTools\Bootstrap',
+	'settings' => [
+		'enable_group' => 'no',
+		'extend_widgets' => 'yes',
+		'extend_activity' => 'no',
+		'mention_display' => 'username',
+		
+	],
 	'widgets' => [
 		'index_thewire' => [
 			'context' => ['index'],
--- views/default/plugins/thewire_tools/settings.php
+++ views/default/plugins/thewire_tools/settings.php
@@ -2,51 +2,43 @@
 
 $plugin = elgg_extract('entity', $vars);
 
-$noyes_options = [
-	'no' => elgg_echo('option:no'),
-	'yes' => elgg_echo('option:yes'),
-];
-
-$display_options = [
-	'username' => elgg_echo('thewire_tools:settings:mention_display:username'),
-	'displayname' => elgg_echo('thewire_tools:settings:mention_display:displayname'),
-];
-
-// enable group support
-$setting = elgg_echo('thewire_tools:settings:enable_group');
-$setting .= elgg_view('input/select', [
+echo elgg_view_field([
+	'#type' => 'checkbox',
+	'#label' => elgg_echo('thewire_tools:settings:enable_group'),
 	'name' => 'params[enable_group]',
-	'value' => $plugin->enable_group,
-	'options_values' => $noyes_options,
-	'class' => 'mlm',
+	'default' => 'no',
+	'value' => 'yes',
+	'checked' => $plugin->enable_group === 'yes',
+	'switch' => true,
 ]);
-echo elgg_format_element('div', [], $setting);
 
-// extend widgets
-$setting = elgg_echo('thewire_tools:settings:extend_widgets');
-$setting .= elgg_view('input/select', [
+echo elgg_view_field([
+	'#type' => 'checkbox',
+	'#label' => elgg_echo('thewire_tools:settings:extend_widgets'),
 	'name' => 'params[extend_widgets]',
-	'value' => $plugin->extend_widgets,
-	'options_values' => array_reverse($noyes_options, true),
-	'class' => 'mlm',
+	'default' => 'no',
+	'value' => 'yes',
+	'checked' => $plugin->extend_widgets === 'yes',
+	'switch' => true,
 ]);
-echo elgg_format_element('div', [], $setting);
 
-// extend widgets
-$setting = elgg_echo('thewire_tools:settings:extend_activity');
-$setting .= elgg_view('input/select', [
+echo elgg_view_field([
+	'#type' => 'checkbox',
+	'#label' => elgg_echo('thewire_tools:settings:extend_activity'),
 	'name' => 'params[extend_activity]',
-	'value' => $plugin->extend_activity,
-	'options_values' => $noyes_options,
-	'class' => 'mlm',
+	'default' => 'no',
+	'value' => 'yes',
+	'checked' => $plugin->extend_activity === 'yes',
+	'switch' => true,
 ]);
-echo elgg_format_element('div', [], $setting);
 
-$setting = elgg_echo('thewire_tools:settings:mention_display');
-$setting .= elgg_view('input/select', [
+echo elgg_view_field([
+	'#type' => 'select',
+	'#label' => elgg_echo('thewire_tools:settings:mention_display'),
 	'name' => 'params[mention_display]',
 	'value' => $plugin->mention_display,
-	'options_values' => $display_options,
-	'class' => 'mlm',
+	'options_values' => [
+		'username' => elgg_echo('thewire_tools:settings:mention_display:username'),
+		'displayname' => elgg_echo('thewire_tools:settings:mention_display:displayname'),
+	],
 ]);
-echo elgg_format_element('div', [], $setting);
