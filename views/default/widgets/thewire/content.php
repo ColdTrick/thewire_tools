84c996adb72ecffef8bfcd1a1b2b8478a174b0e0
Jerome Bakker <jeabakker@coldtrick.com>
2019-01-28 16:08:37 +0100

    fixed: styling and db issues

--- views/default/widgets/thewire/content.php
+++ views/default/widgets/thewire/content.php
@@ -1,75 +1,64 @@
 <?php
-	
-$widget = $vars["entity"];
+
+/* @var $widget ElggWidget */
+$widget = $vars['entity'];
 
 $num_display = (int) $widget->num_display;
 $owner = $widget->owner;
 $filter = $widget->filter;
 
-$error = false;
-
 if ($num_display < 1) {
 	$num_display = 4;
 }
 
-$options = array(
-	"type" => "object",
-	"subtype" => "thewire",
-	"limit" => $num_display,
-	"full_view" => false,
-	"pagination" => false
-);
+$options = [
+	'type' => 'object',
+	'subtype' => 'thewire',
+	'limit' => $num_display,
+	'full_view' => false,
+	'pagination' => false,
+];
 
-$more_url = "";
+$more_url = '';
 switch ($owner) {
-	case "friends":
+	case 'friends':
 		// get users friends
-		$friends_options = array(
-			"type" => "user",
-			"limit" => false,
-			"relationship" => "friend",
-			"relationship_guid" => $widget->getOwnerGUID(),
-			"joins" => array("JOIN " . elgg_get_config("dbprefix") . "entity_relationships r2 ON r2.guid_one = e.guid"),
-			"wheres" => array("(r2.guid_two = " . elgg_get_site_entity()->getGUID() . " AND r2.relationship = 'member_of_site')"),
-			"site_guids" => false
-		);
+		$options['relationship'] = 'friend';
+		$options['relationship_guid'] = $widget->owner_guid;
+		$options['relationship_join_on'] = 'container_guid';
 		
-		$friends = elgg_get_entities_from($friends_options);
-		if (!empty($friends)) {
-			$guids = array();
-			
-			foreach ($friends as $friend) {
-				$guids[] = $friend->getGUID();
-			}
-			
-			$options["container_guids"] = $guids;
-			$more_url = "thewire/friends/" . $widget->getOwnerEntity()->username;
-		} else {
-			$error = true;
-		}
+		$more_url = elgg_generate_url('collection:object:thewire:friends', [
+			'username' => $widget->getOwnerEntity()->username
+		]);
 		break;
-	case "all":
+	case 'all':
 		// show all posts
-		$more_url = "thewire/all";
+		$more_url = elgg_generate_url('collection:object:thewire:all');
 		break;
 	default:
-		$options["container_guid"] = $widget->getOwnerGUID();
-		$more_url = "thewire/owner/" . $widget->getOwnerEntity()->username;
+		$options['container_guid'] = $widget->getOwnerGUID();
+		$more_url = elgg_generate_url('collection:object:thewire:owner', [
+			'username' => $widget->getOwnerEntity()->username
+		]);
 		break;
 }
 
 
-if (empty($error) && !empty($filter)) {
+if (!empty($filter)) {
 	$filters = string_to_tag_array($filter);
-	$filters = array_map("sanitise_string", $filters);
+	$filters = array_map('sanitise_string', $filters);
 	
-	$options["joins"] = array("JOIN " . elgg_get_config("dbprefix") . "objects_entity oe ON oe.guid = e.guid");
-	$options["wheres"] = array("(oe.description LIKE '%" . implode("%' OR oe.description LIKE '%", $filters) . "%')");
+	$options['metadata_name_value_pairs'][] = [
+		'name' => 'description',
+		'value' => '%' . implode('%\', \'%', $filters) . '%',
+		'operand' => 'like',
+		'case_sensitive' => false,
+	];
 }
 
 // show add form in widget
-if (elgg_is_logged_in() && (elgg_get_plugin_setting("extend_widgets", "thewire_tools") != "no")) {
-	echo elgg_view_form("thewire/add");
+if (elgg_is_logged_in() && (elgg_get_plugin_setting('extend_widgets', 'thewire_tools') != 'no')) {
+	echo elgg_view_form('thewire/add');
 }
 
 // list content
@@ -77,13 +66,11 @@ $content = elgg_list_entities($options);
 if (empty($error) && !empty($content)) {
 	echo $content;
 	
-	echo "<span class=\"elgg-widget-more\">";
-	echo elgg_view("output/url", array(
-		"href" => $more_url,
-		"text" => elgg_echo("thewire:moreposts"),
+	echo elgg_format_element('div', ['class' => 'elgg-widget-more'], elgg_view('output/url', [
+		'href' => $more_url,
+		'text' => elgg_echo('thewire:moreposts'),
 		'is_trusted' => true,
-	));
-	echo "</span>";
+	]));
 } else {
-	echo elgg_echo("thewire_tools:no_result");
+	echo elgg_echo('thewire_tools:no_result');
 }
