f2fdf0468c05b23883e69a8fb728527ae22e0f5f
Chris Mitchell <MacMhicheil@users.noreply.github.com>
2021-11-09 10:44:08 +0000

    Add Gaelic language support

---
+++ languages/gd.php
@@ -0,0 +1,78 @@
+<?php
+
+return array(
+	'thewire_tools' => "The Wire Tools",
+	'thewire_tools:no_result' => "Cha deach post-ùeire a lorg",
+	'thewire_tools:login_required' => "Feumaidh tu logadh a-steach gus am feart seo a chleachdadh",
+	'thewire_tools:feature' => "Brosnaich",
+	'thewire_tools:unfeature' => "Na brosnaich tuilleadh",
+
+	// menu
+	'thewire_tools:menu:mentions' => "Iomraidhean",
+
+	// settings
+	'thewire_tools:settings:enable_group' => "Cuir an comas an ùeir ann am buidhnean",
+	'thewire_tools:settings:extend_widgets' => "Leig le postadh ris an ùeir bhon a' mheanbh-aplacaid seo",
+	'thewire_tools:settings:extend_activity' => "Leig le postadh ris an ùeir bho duilleag an inbhir-gnìomhachd",
+
+	// reshare
+	'thewire_tools:notify:reshare:subject' => "Cho-roinn %s %s: %s",
+	'thewire_tools:notify:reshare:summary' => "Cho-roinn %s %s: %s",
+	'thewire_tools:notify:reshare:body' => "Cho-roinn %s na leanas leat:
+
+%s
+
+Briog an seo airson fhaichinn:
+%s
+
+Cuir freagairt ris:
+%s",
+
+	// group wire
+	'thewire_tools:group:title' => "Postaichean-ùeire ann am buidhnean",
+	'thewire_tools:groups:tool_option' => "Cuir an comas postaichean-ùeire ann am buidhnean",
+	'thewire_tools:groups:error:not_enabled' => "Chaidh an ùeir a chur à comas anns a' bhuidhinn seo",
+
+	'thewire_tools:add:container:site' => "Postaich ris an làrach",
+	'thewire_tools:add:access' => "Ri fhaicinn le: %s",
+	'thewire_tools:add:access:group' => "Postaich ri buidheann a-mhàin",
+
+
+	// search
+	'thewire_tools:search:title' => "Lorg an ùeir airson: '%s'",
+	'thewire_tools:search:title:no_query' => "Lorg an ùeir",
+	'thewire_tools:search:no_query' => "Cuir a-steach teacsa gu h-àrd gus lorg an ùeir",
+
+	// reshare
+	'thewire_tools:reshare' => "Postaich ris an ùeir",
+	'thewire_tools:reshare:source' => "Tùs",
+	'thewire_tools:reshare:list' => "Cho-roinn %s seo: %s",
+	'thewire_tools:reshare:list:group' => "Cho-roinn %s seo ris a' bhuidhinn %s %s",
+
+	// featured
+	'thewire_tools:sidebar:featured' => "Postaichean brosnaichte",
+
+	// entity tools
+	'thewire_tools:entity_tools:unable' => "Cha gabh atharrachadh",
+
+	// widget
+	// thewire_groups
+	'widgets:thewire_groups:title' => "Postaichean-ùeire anns a' bhuidhinn",
+	'widgets:thewire_groups:description' => "Seall postaichean-ùeire anns a' bhuidhinn",
+
+	// index_thewire
+	'widgets:index_thewire:name' => "An ùeir",
+	'widgets:index_thewire:description' => "Seall na postaichean-ùeire as ùire air a' choimhearsnachd agad",
+
+	// the wire post
+	'widgets:thewire_post:name' => "Postaich ris an ùeir",
+	'widgets:thewire_post:description' => "Postaich ris an ùeir bhon a' mheanbh-aplacaid seo",
+
+	// the wire (default widget)
+	'widgets:thewire:owner' => "Criathraich na postaichean a-rèir cleachdaiche",
+	'widgets:thewire:filter' => "Criathraich na postaichean-ùeire (roghainneil)",
+
+	// action
+	'thewire_tools:action:toggle_feature:unfeatured' => "Cha tèid am post a bhrosnachadh tuilleadh",
+	'thewire_tools:action:toggle_feature:featured' => "Chaidh am post a bhrosnachadh gu soirbheachail",
+);
